7484b86d06d083956a759a5802d42f062e7f3a9e
Menna Hasan <mennahasan20@gmail.com>
2022-10-30 16:08:11 +0200

    Update events_handeling.cpp

--- events_handeling.cpp
+++ events_handeling.cpp
@@ -1,50 +1,40 @@
 #include <SFML/Graphics.hpp>
+//#include <RectangleShape.hpp>
 #include <iostream>
-
+//using namespace std;
 using namespace sf;
 
-int main(){
+int main()
+{
+   //making the window show up
+   RenderWindow window(VideoMode(900,600), "First Game");
 
-    RenderWindow window(VideoMode(900,700), "First Window");
+   RectangleShape rshape(Vector2f(100,500));
 
+    // loop to be repeated, to keep window open (as main doesn't return this way)
+   while(window.isOpen()){
+       // declaring an event
+       Event ev;
 
-    while(window.isOpen()){
+       while (window.pollEvent(ev)){
+            if(ev.type == Event::Closed){
 
-    Event ev;
-
-    while(window.pollEvent(ev)){
-        if(ev.type == Event::Closed){
-
-        window.close();
-    }
-
-    if(ev.type == Event::GainedFocus){
-
-        std::cout << "the window gained focus\n";
-    }
-
-        if(ev.type == Event::KeyPressed){
+                window.close();
+            }
+            if(ev.type == Event::KeyPressed){
                 if(ev.key.code == Keyboard::Up){
-                    std::cout << "keyboard up button is pressed \n"  ;
+                    std:: cout << ev.key.control << "   " << std::endl;
                 }
-        }
-
-
-        if(ev.type == Event::MouseButtonPressed){
-
-                if(ev.mouseButton.button == Mouse::Right){
-                        std::cout <<  "right mouse clicked\n";
+            }
+            if(ev.type == Event::MouseMoved){
+                    std:: cout << "MOVED    " << std::endl;
+            }
+            if(ev.type == Event::MouseButtonPressed){
+                    if(ev.mouseButton.button == Mouse::Right){
+                    std:: cout << "right mouse click pressed   " << std::endl;
                 }
-        }
-
-
-
-        if(ev.type == Event::MouseEntered){
-                std::cout << "mouse entered the window\n" ;
-
-        }
-    }
-    }
-
-return 0;
+            }
+       }
+   }
+    return EXIT_SUCCESS;
 }
